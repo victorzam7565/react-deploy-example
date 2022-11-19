709a9544a29dad4270066b272908d20665115c5c
victor adrian zambrano tamayo <ingenieriainversazambrano@gmail.com>
2022-11-18 23:29:31 -0500

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,16 @@
+export const tasks = [
+    {
+    id:0,
+    title:"mi primer tarea",
+    description : " mi primer tarea",
+},
+    {
+        id:1,
+        title:"mi segunda tarea",
+        description : " mi segunda tarea",},
+        {
+            id:2,
+            title:"mi tercera  tarea",
+            description : " mi tercera tarea",
+        },
+];
